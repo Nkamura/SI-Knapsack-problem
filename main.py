597ba40f9e1e5971a9bbdbbb5090a71b77fb999c
Gustavo Nakamura <gustavonakamura@alunos.utfpr.edu.br>
2024-10-28 00:23:31 -0300

    Help me

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+# Function created to initialize the Item vector - implement one that gets the data from a .txt file
+# https://awari.com.br/vetor-python-aprenda-a-utilizar-a-linguagem-de-programacao-para-manipular-vetores/
+def askForItens():
+    qntd_itens = int(input("Type the number of items:"))
+    #Create maze with dimension 2xItems    
+    matriz = np.zeros((2, qntd_itens))
+    
+    ## Fill Valor
+    for i in range(qntd_itens):
+        matriz[0][i] = int(input(f"Type the Value for each item {i+1}:"))
+    ## Fill Peso
+    for i in range(qntd_itens):
+        matriz[1, i] = float(input(f"Type the Weight for each item {i+1}: "))
+    return matriz
+
+import numpy as np
+
 def mochilaInicial(n):
     # Gerando valores aleatórios para pesos e valores dos itens
     pesos = np.random.randint(0, 16, size=n)
@@ -12,7 +29,7 @@ def mochilaInicial(n):
 
 def capacidadeMochila(matriz):
     somaTotal = np.sum(matriz[0])
-    percentage = 0.5
+    percentage = 0.35
     capacidade = int(somaTotal * percentage)
     return capacidade
 
@@ -57,6 +74,15 @@ def RandomNeighbor(solution):
     tempSolution[index] = 1 - tempSolution[index]  # inverte o valor de 0 para 1 ou de 1 para 0
     return tempSolution
 
+
+#Quantidade de Itens 
+n = 24
+originalValues = mochilaInicial(n)
+print(originalValues)
+capacity = capacidadeMochila(originalValues)
+print(capacidadeMochila(originalValues))
+
+
 import copy
 def solveKnapsack():
     # Perguntar a capacidade total da Mochila
@@ -72,16 +98,11 @@ def solveKnapsack():
     #[12, 7, 11, 8, 9, 15, 6, 4, 13, 8, 12, 9, 7, 5, 11, 6, 10, 4, 9, 7],
     #[24, 13, 23, 15, 16, 30, 10, 8, 25, 20, 24, 17, 14, 12, 23, 9, 21, 7, 19, 13]
     #])
-    
-    #Quantidade de Itens 
-    n = 10
-    originalValues = mochilaInicial(n)
-    print(originalValues)
-    capacity = capacidadeMochila(originalValues)
-    print(capacidadeMochila(originalValues))
 
     # Definir estados Iniciais - Aleatório
-    currentSolution = initRandom(originalValues.shape[1])
+    #currentSolution = initRandom(originalValues.shape[1])
+    currentSolution = [0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0]
+
     currentValue = calculate_value(currentSolution, originalValues, capacity)
     #print(f"Vetor Inicial: {initialSolution}")
     #print(f"Valor Atual: {currentValue}")
@@ -92,18 +113,23 @@ def solveKnapsack():
 
     # Temperatura inicial
     # Temperatura recebe um valor "aleatório" que vai dimiuindo conforme aumentar o número e iterações
-    temperature = 100000
+    temperature = 1000
     #decreaseFactor = 1
-    decreaseFactor = 0.99
+    decreaseFactor = 0.9
     stopTemp = 5
 
     # Stuck count para "sair" de loops ruins
+    # NAO ALTERAR A TEMPERATURA MANUALMENTE!
     stuckCount = 0
     maxStuckCount = 1000
 
     igualZero = 0 
     totalIteracoes = 0
     countPior = 0
+    lastAcepted = 0
+    lastValueRandom = 0
+    lastMath = 0  
+    lastTemp = 0
 
     # Loop infinito - O critério de parada é se não vai aceitar mais variações ("Temperatura"<=0)
     # Loop infinito - O critério de parada é se não vai aceitar mais variações ("Temperatura"<=0)
@@ -113,6 +139,7 @@ def solveKnapsack():
         valorNeighbor = calculate_value(Neighbor, originalValues, capacity)
         
         if valorNeighbor != 0:    
+            lastSolution = copy.deepcopy(currentSolution)
             # Fit = Próximo - Atual > para ver se é melhor que a atual
             fitness = valorNeighbor - currentValue
             totalIteracoes= totalIteracoes + 1
@@ -121,33 +148,43 @@ def solveKnapsack():
                 # Se for MAIOR: Atual = Próximo
                 currentSolution = copy.deepcopy(Neighbor)
                 currentValue = valorNeighbor
-                lastSolution = copy.deepcopy(currentSolution)
+                lastAcepted = 1
             # Se NÃO for MAIOR
             else:
                 # Se existe a probabilidade de aceitar
-                if random.uniform(0, 1) < math.exp(fitness / temperature):
+                lastValueRandom = random.uniform(0, 1)
+                lastMath = math.exp(fitness / temperature)
+                if lastValueRandom < lastMath:
                     # Então Atual = Próximo
                     countPior = countPior + 1
                     currentSolution = copy.deepcopy(Neighbor)
                     currentValue = valorNeighbor
-                    lastSolution = copy.deepcopy(currentSolution)
+                    lastAcepted = 2
             
             # Guarda o maior
             if currentValue > bestValue:
                 bestValue = currentValue
                 bestSolution = copy.deepcopy(currentSolution)
 
-            temperature *= decreaseFactor
+            temperature = temperature * decreaseFactor
             #temperature = temperature - decreaseFactor
+            lastTemp = temperature
         else:
             igualZero = igualZero + 1
 
-    print(f"Total de iteracoes validas: {totalIteracoes}")
-    print(f"Total de iteracoes invalidas: {igualZero}")
-    print(f"Aceitou o pior: {countPior}")
-    return bestSolution, bestValue, lastSolution
+    print(lastAcepted)
+    #print(f"Total de iteracoes validas: {totalIteracoes}")
+    #print(f"Total de iteracoes invalidas: {igualZero}")
+    #print(f"Aceitou o pior: {countPior}")
+    print(f"random: {lastValueRandom}   lastExp: {lastMath}    lastTemp: {lastTemp}")
+    return bestSolution, bestValue, lastSolution, lastTemp
 
-bestSolution, bestValue, lastSolution = solveKnapsack()
-print(f'Melhor solução: {bestSolution} com valor {bestValue}')
-print(f'Last value: {lastSolution}')
+
+bestSolution, bestValue, lastSolution, lastTemp= solveKnapsack()
 print(f'Sao iguais? {checkMatriz(bestSolution,lastSolution)}')
+print(f'Melhor valor: valor {bestValue}')
+totalLast = calculate_value(lastSolution, originalValues, capacity)
+print(f'total last value: {totalLast}')
+print(f'{lastSolution} Last value')
+print(f'{bestSolution} Melhor solução')
+print(f'{lastTemp:.2f} Ultima temp')
